8870e3310360827ae0f971730fd47c89271c7250
apetera3 <petera3@gmatc.matc.edu>
2024-02-12 09:36:42 +0300

    Completed Activity 3

--- blog/src/templates/blog-post.js
+++ blog/src/templates/blog-post.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import Layout from '../components/layout'
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
 
 const BlogPost = ({ data }) => {
-  const { title } = data.contentfulBlogPost
+  const { title, body } = data.contentfulBlogPost
 
   return (
     <Layout>
       <h1>{title}</h1>
+      <div
+        dangerouslySetInnerHTML={{ __html: body.childMarkdownRemark.html }}
+      ></div>
     </Layout>
   )
 }
@@ -16,9 +19,14 @@ export default BlogPost
 
 export const pageQuery = graphql`
   query blogPostQuery($slug: String!) {
-    contentfulBlogPost(slug: {eq: $slug}) {
-      title 
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
       slug
+      body {
+        childMarkdownRemark {
+          html
+        }
+      }
     }
   }
 `
